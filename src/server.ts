1b2f9e83cd5fd001d895598e7b03c41889907d67
atiq9se <atiq9se@gmail.com>
2025-11-30 22:34:40 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,77 @@
+
+import { Server } from "http";
+import mongoose = require("mongoose");
+import app = require("./app");
+
+let server: Server;
+
+const startServer = async () => {
+     try{
+        await mongoose.connect("mongodb://localhost:27017")
+
+        console.log("connected to DB");
+
+        server = app.listen(5000, ()=> {
+            console.log("Server is listenting to port 5000")
+        });
+     } catch(error){
+        console.log(error);
+     }
+}
+startServer()
+
+process.on("unhandledRejection", ()=>{
+    console.log("Unhandled Rejection detected... Server shutting down...");
+
+    if(server){
+        server.close(()=>{
+           process.exit(1)
+        });
+    }
+
+    process.exit()
+})
+
+process.on("uncaughtException", ()=>{
+    console.log("Unhandled Rejection detected... Server shutting down...");
+
+    if(server){
+        server.close(()=>{
+           process.exit(1)
+        });
+    }
+
+    process.exit()
+})
+
+
+process.on("SIGTERM", ()=>{
+    console.log("SIGTERM signal recieved... Server shutting down...");
+
+    if(server){
+        server.close(()=>{
+           process.exit(1)
+        });
+    }
+
+    process.exit()
+})
+
+process.on("SIGINT", ()=>{
+    console.log("SIGINT signal recieved... Server shutting down...");
+
+    if(server){
+        server.close(()=>{
+           process.exit(1)
+        });
+    }
+
+    process.exit()
+})
+
+
+// unhandle rejection error
+// Promise.reject(new Error("I forgot to catch this promise"))
+
+// uncaught exception error
+// throw new Error("I forgot to handle this local error")

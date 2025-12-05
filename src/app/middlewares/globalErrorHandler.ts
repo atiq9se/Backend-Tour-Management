e6997cd51bbfe80f32c73f134af347df8c75663f
atiq9se <atiq9se@gmail.com>
2025-12-05 08:55:49 +0600

    create custom Error class AppError

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,10 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import env = require("../config/env")
+import AppError from "../errorHelpers/AppError";
 
 
 export const globalErrorHandler = (err: any, req: e.Request, res: Response, next: NextFunction)=> {
-    const statusCode = 500
-    const message = `Something went wrong!! ${err.message}`
+    let statusCode = 500
+    let message = `Something went wrong!!`
+
+    if(err instanceof AppError){
+        statusCode = err.statusCode
+        message = err.message
+    }else if (err instanceof Error) {
+        statusCode = 500;
+        message = err.message
+    }
 
     res.status(statusCode).json({
         success: false,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import httpStatus from "http-status-codes";
 import userService = require("./user.service");
+import AppError from "../../errorHelpers/AppError";
 
 const createUser = async(req: Request, res: Response, next: NextFunction) => {
     try{
+        // throw new Error("Fake error normal")
+        // throw new AppError(httpStatus.BAD_REQUEST, "FAKE ERROR")
+
        const user = await userService.UserServices.createUser(req.body)
 
         res.status(httpStatus.CREATED).json({

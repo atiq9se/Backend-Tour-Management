d3952aff20017e41a3ac0a8ea2adaef5ed9b4909
atiq9se <atiq9se@gmail.com>
2025-12-08 08:15:03 +0600

    get all user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,25 +2,54 @@ import { Request, Response, NextFunction } from "express";
 import httpStatus from "http-status-codes";
 import userService = require("./user.service");
 import AppError from "../../errorHelpers/AppError";
+import UserServices = require("./user.service");
 
-const createUser = async(req: Request, res: Response, next: NextFunction) => {
-    try{
-        // throw new Error("Fake error normal")
-        // throw new AppError(httpStatus.BAD_REQUEST, "FAKE ERROR")
+type AsyncHandler = (req: Request, res: Response, next: NextFunction)=> Promise<void>
 
-       const user = await userService.UserServices.createUser(req.body)
+const catchAsync = (fn: AsyncHandler)=> (req: Request, res: Response, next: NextFunction)=>{
+    Promise.resolve(fn(req, res, next)).catch((err: any)=>{
+        console.log(err);
+        next(err)
+    })
+}
 
-        res.status(httpStatus.CREATED).json({
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction)=>{
+    const user = await userService.UserServices.createUser(req.body)
+    res.status(httpStatus.CREATED).json({
             message: "User Created Success Fully",
             user
-        })
-    } catch (err: any){
-        console.log(err);
-        next(err);
+    })
+})
+
+// const createUser = async(req: Request, res: Response, next: NextFunction) => {
+//     try{
+//         // throw new Error("Fake error normal")
+//         // throw new AppError(httpStatus.BAD_REQUEST, "FAKE ERROR")
+
+//        const user = await userService.UserServices.createUser(req.body)
+
+//         res.status(httpStatus.CREATED).json({
+//             message: "User Created Success Fully",
+//             user
+//         })
+//     } catch (err: any){
+//         console.log(err);
+//         next(err);
+//     }
+// }
+
+const getAllUsers = async (req: Request, res: Response, next: NextFunction)=>{
+    try{
+        const users = await userService.UserServices.getAllUsers();
+        return users;
+    }catch(err:any){
+        console.log(err)
+        next(err)
     }
 }
 
 
 export const UserControllers = {
-    createUser
+    createUser,
+    getAllUsers
 }

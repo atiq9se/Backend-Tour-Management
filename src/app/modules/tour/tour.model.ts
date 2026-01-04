3a88ab01203872a37d0e20ec2eee5a7e3a9572e2
atiq9se <atiq9se@gmail.com>
2026-01-04 22:24:51 +0600

    Create Model for tour and tour tpes

--- src/app/modules/tour/tour.model.ts
+++ src/app/modules/tour/tour.model.ts
@@ -1,6 +1,42 @@
-import mongoose = require("mongoose");
-import type tourInterface = require("./tour.interface");
+import { Schema, model } from "mongoose";
+import { ITour, ITourType } from "./tour.interface";
 
-const tourSchema = new mongoose.Schema<tourInterface.ITour>({
-    
+
+const tourTypeSchema = new Schema <ITourType>({
+    name      : {type: String, required: true, unique: true}
+},{
+    timestamps: true
 })
+
+export const TourType = model<ITourType>("TourType", tourTypeSchema)
+
+const tourSchema = new Schema<ITour>({
+    title       : { type: String, required: true},
+    slug        : {type: String, required: true, unique: true},
+    description : {type: String },
+    images      : {type: [String], default: []},
+    location    : {type: String},
+    costFrom    : {type: Number},
+    startDate   : Date,
+    endDate     : Date,
+    included    : {type: [String], default: []},
+    excluded    : {type: [String], default: []},
+    amenities   : {type: [String], default: []},
+    tourPlan    : {type: [String], default: []},
+    maxGuest    : {type: Number},
+    minAge      : {type: Number},
+    division    : {
+                    type: Schema.Types.ObjectId,
+                    ref: "Division",
+                    required: true
+                },
+    tourType    : {
+                    type: Schema.Types.ObjectId,
+                    ref: "TourType",
+                    required: true
+                }
+},{
+    timestamps : true
+})
+
+export const Tour = model<ITour>("Tour", tourSchema)

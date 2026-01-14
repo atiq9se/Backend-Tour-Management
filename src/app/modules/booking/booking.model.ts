d43380764adf40737be154a8fc8cfce620599636
atiq9se <atiq9se@gmail.com>
2026-01-14 22:35:53 +0600

    booking and payment interface and model part create

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,36 @@
+import { Schema } from "mongoose";
+import { IBooking } from "./booking.interface";
+import {BOOKING_STATUS } from "./booking.interface";
+import { model } from "mongoose";
+
+const bookingSchema = new Schema<IBooking>({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    tour: {
+        type: Schema.Types.ObjectId,
+        ref: "Tour",
+        required: true,
+    },
+    payment: {
+        type: Schema.Types.ObjectId,
+        ref: "Payment",
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: Object.values(BOOKING_STATUS),
+        default: BOOKING_STATUS.PENDING
+    },
+    guestCount: {
+        type: Number,
+        required: true
+    }
+
+},{
+    timestamps: true
+})
+
+export const Booking = model<IBooking>("Booking", bookingSchema)
--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -25,6 +25,46 @@ const createTour = async (payload: ITour) => {
     return tour;
 };
 
+
+const getAllTours = async(query: Record<string, string>)=>{
+    const filter = query;
+    const searchTerm = query.searchTerm || "";
+    const sort = query.sort || "-createdAt"
+     const page = query.page || 1
+     const limit = query.limit || 10
+     const skip = (number(page)-1) * Number(limit)
+
+    const fields = query.fields?.split(",").join(" ") || ""
+
+    //  delete filter["searchTerm"]
+    //  delete filter["sort"]
+
+     const excludeField = ["searchTerm", "sort"]
+
+     for(const field of excludefield){
+        delete filter[field]
+     }
+
+    const tourSearchableFields = ["title", "description", "location"]
+
+    const searchArray = {
+        $or: tourSearchableFields.map(field=> ({ [field]: {$regex: searchTerm, $options: "i"} }))
+    }
+
+    const tours = await Tour.find(searchArray).find(filter).sort("-location").select("title");
+
+    const totalTours = await Tour.countDocuments();
+
+    return{
+        data: tours,
+        meta: {
+            total: totalTours
+        }
+    }
+    
+}
+
+
 // const getAllToursOld = async (query: Record<string, string>) => {
 //     console.log(query);
 //     const filter = query
